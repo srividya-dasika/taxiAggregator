dbeb6bc59c2edce6cb638a1053c0942946536556
srividya <srividya.dasika@gmail.com>
2021-12-06 09:42:29 +0530

    Taxi Simulator code writing taxi coords into Mongo

--- src/TaxiSimulator/TaxiSimulatorClient/taxiSimulation.py
+++ src/TaxiSimulator/TaxiSimulatorClient/taxiSimulation.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 
-def processdata():
+def processdata(fileToProcess):
     # Loading and reading the CSV file.
-    df = pd.read_csv('../../config/chipotle_stores.csv', delimiter =',')
+    df = pd.read_csv(fileToProcess, delimiter =',')
     # Converting the CSV data to a list.
     df = df.to_dict(orient = 'records')
     return df
